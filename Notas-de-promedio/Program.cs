ccb3efe1d8e18f436ef043f47c1a1924e61bcf78
Josueidiaquez <josueidiaquez4@gmail.com>
2024-08-15 19:57:31 -0600

    Agregar archivos de proyecto.

---
+++ Notas-de-promedio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Notas-de-promedio
+{
+    public class Program
+    {
+      public static void Main(string[] args)
+      {
+        int ap = 0;
+        float nota, Aprobadas, Reprobadas = 0;
+        for (int x = 0; x < 40; x++)
+        {
+            Console.WriteLine("Favor Ingresar Nota");
+            nota = float.Parse(Console.ReadLine());
+            if (nota >= 3)
+            {
+                Aprobadas = Aprobadas + nota;
+            }
+            else { 
+            Reprobadas = Reprobadas + nota;
+            }
+            Console.WriteLine("El promedio de las notas Aprobadas es: " + Aprobadas / ap);
+            Console.WriteLine("El promedio de las notas Reprobadas es: " + Reprobadas / (40 - ap));
+        }
+      }
+    }
+}
